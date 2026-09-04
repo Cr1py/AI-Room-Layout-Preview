cd295d66ea31e87f67ddb77c84586f372b445f1b
Christine Bautista <c.bautista2503@gmail.com>
2026-09-04 01:36:20 -0700

    added basic upload section (no logic yet)

--- src/components/upload.tsx
+++ src/components/upload.tsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+import { UploadIcon } from 'lucide-react';
 
 const Upload = () => {
+  const [file, setFile] = useState<File | null>(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const [progress, setProgress] = useState(0);
+
+
   return (
-    <div> Upload Test </div>
-    )
-}
-export default Upload
+    <div className="upload">
+      {!file ? (
+        <div className={`upload-dropzone ${isDragging ? 'dragging' : ''}`}>
+          <input
+            type="file"
+            className="drop-input"
+            accept=".jpeg, .png, .webp"
+          />
+
+          <div className="drop-content">
+            <div className="drop-icon">
+              <UploadIcon size={20} lightingColor="bg-grey-dark"/>
+            </div>
+          </div>
+
+        </div>
+      ) : (
+        <div className="upload-file"> </div>
+      )}  
+    </div>
+  );
+};
+
+export default Upload;
